eb43fe7fa02aa5c73139f3439640660b48510e70
DoosDoos <boymansjoes@gmail.com>
2022-07-02 13:29:02 +0200

    Form aanpassingen

--- Monopoly/frmPlayQuiz.Designer.cs
+++ Monopoly/frmPlayQuiz.Designer.cs
@@ -29,14 +29,6 @@ namespace Monopoly
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblColorRed = new System.Windows.Forms.Label();
-            this.lblColorGreen = new System.Windows.Forms.Label();
-            this.lblColorYellow = new System.Windows.Forms.Label();
-            this.lblColorBlue = new System.Windows.Forms.Label();
-            this.txtbColorRedName = new System.Windows.Forms.TextBox();
-            this.txtbColorGreenName = new System.Windows.Forms.TextBox();
-            this.txtbColorYellowName = new System.Windows.Forms.TextBox();
-            this.txtbColorBlueName = new System.Windows.Forms.TextBox();
             this.lblClose = new System.Windows.Forms.Label();
             this.lblSelectedCategory = new System.Windows.Forms.Label();
             this.cboxCategories = new System.Windows.Forms.ComboBox();
@@ -45,129 +37,43 @@ namespace Monopoly
             this.btnCreateGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblColorRed
-            // 
-            this.lblColorRed.AutoSize = true;
-            this.lblColorRed.BackColor = System.Drawing.Color.Transparent;
-            this.lblColorRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColorRed.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblColorRed.Location = new System.Drawing.Point(41, 221);
-            this.lblColorRed.Name = "lblColorRed";
-            this.lblColorRed.Size = new System.Drawing.Size(78, 32);
-            this.lblColorRed.TabIndex = 2;
-            this.lblColorRed.Text = "Red:";
-            // 
-            // lblColorGreen
-            // 
-            this.lblColorGreen.AutoSize = true;
-            this.lblColorGreen.BackColor = System.Drawing.Color.Transparent;
-            this.lblColorGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColorGreen.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblColorGreen.Location = new System.Drawing.Point(215, 221);
-            this.lblColorGreen.Name = "lblColorGreen";
-            this.lblColorGreen.Size = new System.Drawing.Size(107, 32);
-            this.lblColorGreen.TabIndex = 3;
-            this.lblColorGreen.Text = "Green:";
-            // 
-            // lblColorYellow
-            // 
-            this.lblColorYellow.AutoSize = true;
-            this.lblColorYellow.BackColor = System.Drawing.Color.Transparent;
-            this.lblColorYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColorYellow.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblColorYellow.Location = new System.Drawing.Point(41, 326);
-            this.lblColorYellow.Name = "lblColorYellow";
-            this.lblColorYellow.Size = new System.Drawing.Size(114, 32);
-            this.lblColorYellow.TabIndex = 4;
-            this.lblColorYellow.Text = "Yellow:";
-            // 
-            // lblColorBlue
-            // 
-            this.lblColorBlue.AutoSize = true;
-            this.lblColorBlue.BackColor = System.Drawing.Color.Transparent;
-            this.lblColorBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColorBlue.Location = new System.Drawing.Point(215, 326);
-            this.lblColorBlue.Name = "lblColorBlue";
-            this.lblColorBlue.Size = new System.Drawing.Size(85, 32);
-            this.lblColorBlue.TabIndex = 5;
-            this.lblColorBlue.Text = "Blue:";
-            // 
-            // txtbColorRedName
-            // 
-            this.txtbColorRedName.Location = new System.Drawing.Point(46, 259);
-            this.txtbColorRedName.Name = "txtbColorRedName";
-            this.txtbColorRedName.Size = new System.Drawing.Size(110, 29);
-            this.txtbColorRedName.TabIndex = 6;
-            // 
-            // txtbColorGreenName
-            // 
-            this.txtbColorGreenName.Location = new System.Drawing.Point(220, 259);
-            this.txtbColorGreenName.Name = "txtbColorGreenName";
-            this.txtbColorGreenName.Size = new System.Drawing.Size(110, 29);
-            this.txtbColorGreenName.TabIndex = 6;
-            // 
-            // txtbColorYellowName
-            // 
-            this.txtbColorYellowName.Location = new System.Drawing.Point(46, 365);
-            this.txtbColorYellowName.Name = "txtbColorYellowName";
-            this.txtbColorYellowName.Size = new System.Drawing.Size(110, 29);
-            this.txtbColorYellowName.TabIndex = 6;
-            // 
-            // txtbColorBlueName
-            // 
-            this.txtbColorBlueName.Location = new System.Drawing.Point(220, 365);
-            this.txtbColorBlueName.Name = "txtbColorBlueName";
-            this.txtbColorBlueName.Size = new System.Drawing.Size(110, 29);
-            this.txtbColorBlueName.TabIndex = 6;
-            // 
             // lblClose
             // 
             this.lblClose.AutoSize = true;
             this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblClose.ForeColor = System.Drawing.Color.Red;
-            this.lblClose.Location = new System.Drawing.Point(11, 9);
+            this.lblClose.Location = new System.Drawing.Point(12, 9);
             this.lblClose.Name = "lblClose";
-            this.lblClose.Size = new System.Drawing.Size(25, 24);
+            this.lblClose.Size = new System.Drawing.Size(27, 25);
             this.lblClose.TabIndex = 7;
             this.lblClose.Text = "X";
             this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlay.Location = new System.Drawing.Point(43, 422);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(287, 116);
-            this.btnPlay.TabIndex = 0;
-            this.btnPlay.Text = "Play Game";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // lblSelectedCategory
             // 
             this.lblSelectedCategory.AutoSize = true;
             this.lblSelectedCategory.BackColor = System.Drawing.Color.Transparent;
             this.lblSelectedCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSelectedCategory.Location = new System.Drawing.Point(50, 14);
+            this.lblSelectedCategory.Location = new System.Drawing.Point(55, 15);
             this.lblSelectedCategory.Name = "lblSelectedCategory";
-            this.lblSelectedCategory.Size = new System.Drawing.Size(268, 24);
+            this.lblSelectedCategory.Size = new System.Drawing.Size(300, 25);
             this.lblSelectedCategory.TabIndex = 8;
             this.lblSelectedCategory.Text = "**SELECTED CATEGORY**";
             // 
             // cboxCategories
             // 
             this.cboxCategories.FormattingEnabled = true;
-            this.cboxCategories.Location = new System.Drawing.Point(46, 56);
+            this.cboxCategories.Location = new System.Drawing.Point(50, 58);
             this.cboxCategories.Name = "cboxCategories";
-            this.cboxCategories.Size = new System.Drawing.Size(283, 32);
+            this.cboxCategories.Size = new System.Drawing.Size(308, 33);
             this.cboxCategories.TabIndex = 9;
             this.cboxCategories.SelectedIndexChanged += new System.EventHandler(this.cboxCategories_SelectedIndexChanged);
             // 
             // txtbGameName
             // 
-            this.txtbGameName.Location = new System.Drawing.Point(43, 154);
+            this.txtbGameName.Location = new System.Drawing.Point(47, 160);
             this.txtbGameName.Name = "txtbGameName";
-            this.txtbGameName.Size = new System.Drawing.Size(287, 29);
+            this.txtbGameName.Size = new System.Drawing.Size(313, 31);
             this.txtbGameName.TabIndex = 10;
             // 
             // lblGameName
@@ -175,18 +81,18 @@ namespace Monopoly
             this.lblGameName.AutoSize = true;
             this.lblGameName.BackColor = System.Drawing.Color.Transparent;
             this.lblGameName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.lblGameName.Location = new System.Drawing.Point(37, 115);
+            this.lblGameName.Location = new System.Drawing.Point(40, 120);
             this.lblGameName.Name = "lblGameName";
-            this.lblGameName.Size = new System.Drawing.Size(172, 32);
+            this.lblGameName.Size = new System.Drawing.Size(195, 37);
             this.lblGameName.TabIndex = 11;
             this.lblGameName.Text = "Spel Naam:";
             // 
             // btnCreateGame
             // 
             this.btnCreateGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateGame.Location = new System.Drawing.Point(47, 440);
+            this.btnCreateGame.Location = new System.Drawing.Point(49, 210);
             this.btnCreateGame.Name = "btnCreateGame";
-            this.btnCreateGame.Size = new System.Drawing.Size(313, 121);
+            this.btnCreateGame.Size = new System.Drawing.Size(309, 126);
             this.btnCreateGame.TabIndex = 0;
             this.btnCreateGame.Text = "Spel Aanmaken";
             this.btnCreateGame.UseVisualStyleBackColor = true;
@@ -194,23 +100,15 @@ namespace Monopoly
             // 
             // frmPlayQuiz
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Monopoly.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(374, 576);
+            this.ClientSize = new System.Drawing.Size(408, 380);
             this.Controls.Add(this.lblGameName);
             this.Controls.Add(this.txtbGameName);
             this.Controls.Add(this.cboxCategories);
             this.Controls.Add(this.lblSelectedCategory);
             this.Controls.Add(this.lblClose);
-            this.Controls.Add(this.txtbColorBlueName);
-            this.Controls.Add(this.txtbColorYellowName);
-            this.Controls.Add(this.txtbColorGreenName);
-            this.Controls.Add(this.txtbColorRedName);
-            this.Controls.Add(this.lblColorBlue);
-            this.Controls.Add(this.lblColorYellow);
-            this.Controls.Add(this.lblColorGreen);
-            this.Controls.Add(this.lblColorRed);
             this.Controls.Add(this.btnCreateGame);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPlayQuiz";
@@ -226,14 +124,6 @@ namespace Monopoly
         }
 
         #endregion
-        private System.Windows.Forms.Label lblColorRed;
-        private System.Windows.Forms.Label lblColorGreen;
-        private System.Windows.Forms.Label lblColorYellow;
-        private System.Windows.Forms.Label lblColorBlue;
-        private System.Windows.Forms.TextBox txtbColorRedName;
-        private System.Windows.Forms.TextBox txtbColorGreenName;
-        private System.Windows.Forms.TextBox txtbColorYellowName;
-        private System.Windows.Forms.TextBox txtbColorBlueName;
         private System.Windows.Forms.Label lblClose;
         private System.Windows.Forms.Label lblSelectedCategory;
         private System.Windows.Forms.ComboBox cboxCategories;
--- Monopoly/frmQuestionOverview.Designer.cs
+++ Monopoly/frmQuestionOverview.Designer.cs
@@ -29,7 +29,7 @@ namespace Monopoly
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvQuestions = new System.Windows.Forms.DataGridView();
             this.GameQuestion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.GameAnswer = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -57,7 +57,7 @@ namespace Monopoly
             this.GameQuestion,
             this.GameAnswer,
             this.Delete});
-            this.dgvQuestions.Location = new System.Drawing.Point(37, 42);
+            this.dgvQuestions.Location = new System.Drawing.Point(40, 44);
             this.dgvQuestions.Margin = new System.Windows.Forms.Padding(4);
             this.dgvQuestions.Name = "dgvQuestions";
             this.dgvQuestions.ReadOnly = true;
@@ -65,7 +65,7 @@ namespace Monopoly
             this.dgvQuestions.RowHeadersWidth = 82;
             this.dgvQuestions.RowTemplate.Height = 33;
             this.dgvQuestions.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvQuestions.Size = new System.Drawing.Size(1771, 414);
+            this.dgvQuestions.Size = new System.Drawing.Size(1248, 431);
             this.dgvQuestions.TabIndex = 1;
             this.dgvQuestions.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvQuestions_CellClick);
             // 
@@ -87,10 +87,10 @@ namespace Monopoly
             // 
             // Delete
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Firebrick;
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Firebrick;
-            this.Delete.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Firebrick;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Firebrick;
+            this.Delete.DefaultCellStyle = dataGridViewCellStyle4;
             this.Delete.HeaderText = "Delete Question";
             this.Delete.MinimumWidth = 10;
             this.Delete.Name = "Delete";
@@ -101,50 +101,52 @@ namespace Monopoly
             // lblQuestionName
             // 
             this.lblQuestionName.AutoSize = true;
+            this.lblQuestionName.BackColor = System.Drawing.Color.Transparent;
             this.lblQuestionName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuestionName.Location = new System.Drawing.Point(46, 508);
+            this.lblQuestionName.Location = new System.Drawing.Point(34, 479);
             this.lblQuestionName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblQuestionName.Name = "lblQuestionName";
-            this.lblQuestionName.Size = new System.Drawing.Size(183, 29);
+            this.lblQuestionName.Size = new System.Drawing.Size(205, 31);
             this.lblQuestionName.TabIndex = 2;
             this.lblQuestionName.Text = "Question Text:";
             // 
             // lblQuestionAnswer
             // 
             this.lblQuestionAnswer.AutoSize = true;
+            this.lblQuestionAnswer.BackColor = System.Drawing.Color.Transparent;
             this.lblQuestionAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuestionAnswer.Location = new System.Drawing.Point(915, 518);
+            this.lblQuestionAnswer.Location = new System.Drawing.Point(34, 600);
             this.lblQuestionAnswer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblQuestionAnswer.Name = "lblQuestionAnswer";
-            this.lblQuestionAnswer.Size = new System.Drawing.Size(218, 29);
+            this.lblQuestionAnswer.Size = new System.Drawing.Size(245, 31);
             this.lblQuestionAnswer.TabIndex = 3;
             this.lblQuestionAnswer.Text = "Question Answer:";
             // 
             // txtbQuestionName
             // 
-            this.txtbQuestionName.Location = new System.Drawing.Point(51, 566);
+            this.txtbQuestionName.Location = new System.Drawing.Point(40, 511);
             this.txtbQuestionName.Margin = new System.Windows.Forms.Padding(4);
             this.txtbQuestionName.Multiline = true;
             this.txtbQuestionName.Name = "txtbQuestionName";
-            this.txtbQuestionName.Size = new System.Drawing.Size(803, 58);
+            this.txtbQuestionName.Size = new System.Drawing.Size(756, 60);
             this.txtbQuestionName.TabIndex = 4;
             // 
             // txtbQuestionAnswer
             // 
-            this.txtbQuestionAnswer.Location = new System.Drawing.Point(920, 566);
+            this.txtbQuestionAnswer.Location = new System.Drawing.Point(40, 635);
             this.txtbQuestionAnswer.Margin = new System.Windows.Forms.Padding(4);
             this.txtbQuestionAnswer.Multiline = true;
             this.txtbQuestionAnswer.Name = "txtbQuestionAnswer";
-            this.txtbQuestionAnswer.Size = new System.Drawing.Size(220, 58);
+            this.txtbQuestionAnswer.Size = new System.Drawing.Size(758, 60);
             this.txtbQuestionAnswer.TabIndex = 5;
             // 
             // btnAddQuestion
             // 
             this.btnAddQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddQuestion.Location = new System.Drawing.Point(1255, 548);
+            this.btnAddQuestion.Location = new System.Drawing.Point(867, 557);
             this.btnAddQuestion.Margin = new System.Windows.Forms.Padding(4);
             this.btnAddQuestion.Name = "btnAddQuestion";
-            this.btnAddQuestion.Size = new System.Drawing.Size(156, 87);
+            this.btnAddQuestion.Size = new System.Drawing.Size(180, 100);
             this.btnAddQuestion.TabIndex = 6;
             this.btnAddQuestion.Text = "Add Question";
             this.btnAddQuestion.UseVisualStyleBackColor = true;
@@ -155,10 +157,10 @@ namespace Monopoly
             this.lblClose.AutoSize = true;
             this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblClose.ForeColor = System.Drawing.Color.Red;
-            this.lblClose.Location = new System.Drawing.Point(11, 9);
+            this.lblClose.Location = new System.Drawing.Point(12, 9);
             this.lblClose.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblClose.Name = "lblClose";
-            this.lblClose.Size = new System.Drawing.Size(25, 24);
+            this.lblClose.Size = new System.Drawing.Size(27, 25);
             this.lblClose.TabIndex = 7;
             this.lblClose.Text = "X";
             this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
@@ -166,10 +168,10 @@ namespace Monopoly
             // btnEditQuestion
             // 
             this.btnEditQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditQuestion.Location = new System.Drawing.Point(1533, 508);
-            this.btnEditQuestion.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEditQuestion.Location = new System.Drawing.Point(1108, 557);
+            this.btnEditQuestion.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.btnEditQuestion.Name = "btnEditQuestion";
-            this.btnEditQuestion.Size = new System.Drawing.Size(312, 166);
+            this.btnEditQuestion.Size = new System.Drawing.Size(180, 100);
             this.btnEditQuestion.TabIndex = 6;
             this.btnEditQuestion.Text = "Edit Question";
             this.btnEditQuestion.UseVisualStyleBackColor = true;
@@ -178,21 +180,22 @@ namespace Monopoly
             // lblSelectedCategory
             // 
             this.lblSelectedCategory.AutoSize = true;
+            this.lblSelectedCategory.BackColor = System.Drawing.Color.Transparent;
             this.lblSelectedCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSelectedCategory.Location = new System.Drawing.Point(744, 9);
-            this.lblSelectedCategory.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSelectedCategory.Location = new System.Drawing.Point(480, 9);
+            this.lblSelectedCategory.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.lblSelectedCategory.Name = "lblSelectedCategory";
-            this.lblSelectedCategory.Size = new System.Drawing.Size(315, 29);
+            this.lblSelectedCategory.Size = new System.Drawing.Size(354, 31);
             this.lblSelectedCategory.TabIndex = 8;
             this.lblSelectedCategory.Text = "**CHOSEN CATEGORY**\r\n";
             // 
             // frmQuestionOverview
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
             this.BackgroundImage = global::Monopoly.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(1903, 731);
+            this.ClientSize = new System.Drawing.Size(1332, 737);
             this.Controls.Add(this.lblSelectedCategory);
             this.Controls.Add(this.lblClose);
             this.Controls.Add(this.btnEditQuestion);
